939c353076bf51551bc3cbf7fb16a5170f804cad
Nikolaos Triantafyllis <ntriantafyl@cslab.ece.ntua.gr>
2023-09-29 01:25:32 +0200

    Fixing cpu handling in resource creation

--- oar/lib/resource_handling.py
+++ oar/lib/resource_handling.py
@@ -522,13 +522,13 @@ def get_count_busy_resources(
 
 
 def resources_creation(session, node_name, nb_nodes, nb_core=1, nb_cpu=1, vfactor=1):
-    for i in range(nb_nodes * nb_cpu * nb_core * vfactor):
+    for i in range(nb_nodes * nb_core * vfactor):
         Resource.create(
             session,
             network_address=f"{node_name}{int(i/(nb_core * vfactor)+1)}",
             cpuset=i % nb_core,
             core=i+1,
-            cpu=i//nb_core+1,
+            cpu=i//(nb_core//nb_cpu)+1,
             state="Alive",
         )
     session.commit()
