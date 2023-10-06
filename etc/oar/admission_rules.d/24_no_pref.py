830b89e7dbeda345ebdd6d5f1d017abcae287f1f
Stratos Karapanagiotis <skarapan@cslab.ece.ntua.gr>
2023-10-06 15:21:17 +0300

    Fixing admission rules types inference error

--- etc/oar/admission_rules.d/24_no_pref.py
+++ etc/oar/admission_rules.d/24_no_pref.py
@@ -2,6 +2,6 @@
 if 'no_pref' in types:
     types = list(map(lambda t: t.replace('no_pref','find=no_pref'),types))
 
-if not (('find=compact' in types) or ('find=spread' in types) or ('find=no_pref' in types)):
-    types.append('find=no_pref')
+# if not (('find=compact' in types) or ('find=spread' in types) or ('find=no_pref' in types)):
+#     types.append('find=no_pref')
 
