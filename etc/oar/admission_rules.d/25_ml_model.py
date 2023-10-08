b26fa18cc7d1cac679ed46bdc7899d3d2ab13d29
Stratos Karapanagiotis <skarapan@cslab.ece.ntua.gr>
2023-10-08 21:12:09 +0300

    Added autoprofiling based on the perf codes of the machine

--- etc/oar/admission_rules.d/25_ml_model.py
+++ etc/oar/admission_rules.d/25_ml_model.py
@@ -3,7 +3,6 @@
 print(resource_request, properties) # debug
 print(f"Types from previous admission rules = {types}")
 
-
 def model(resource_request, properties, command):
     """
     Return: String
@@ -15,23 +14,42 @@ def model(resource_request, properties, command):
     from joblib import load
     import pandas as pd
     import numpy as np
+    import re
+    from os import path
 
-    # Import the NAS 'database'
-    db = pd.read_csv('/etc/oar/admission_rules.d/nas-oar-db.csv')
+    # Import the NAS/SPEC 'database'
+    db = pd.read_csv('/etc/oar/admission_rules.d/nas-spec-oar-db.csv')
 
-    # Check if it is a NAS benchmark
-    # bench = command.split('.')
-    # if len(bench) < 3:
-    #     print("We have to gather perf counters")
-    #     return type_from_ml
-    # else:
-    #     app, cls, procs = bench
+    print(f"BEFORE>{command}")
+
+    # Remove the mpirun call
+    command = command.replace("mpirun", "")
+    command = command.replace("perf stat", "")
+
+    # Find all the arguments passed to mpirun
+    # and the executable
+    m = re.findall(r"-+\w+\s*\w*", command)
+    for string in m:
+        command = command.replace(string, "")
+
+    # Remove all the whitespaces
+    command = command.strip()
+
+    # Return the basename of the executable
+    name = path.basename(command)
+    name = name.lstrip().rstrip()
+
+    print()
+    print(f"NOW>{name}")
+    print()
 
     # Check if the executable's name exists inside the db
-    indices = db[db['name'].str.contains(command)].index.values
+    indices = db[db['name'].str.contains(name)].index.values
 
     if len(indices) == 0:
         print("The executable is not inside the database")
+        print("We will collect the necessary performance counters")
+        return "profiling"
         vector = np.zeros((1, 12))
     else:
         print(f"{command} exists inside the db!")
@@ -77,6 +95,31 @@ if ('find=compact' not in types) and ('find=spread' not in types) and ('find=no_
     # type_from_ml can be: "find=compact" or "find=spread" or "find=no_pref"
     type_from_ml = model(resource_request, properties, command)
 
+    if type_from_ml == "profiling":
+
+        perf_str = "perf stat "
+
+        fd = open("/etc/perf_codes", "r")
+
+        # Skip csv header line
+        for line in fd.readlines()[1:]:
+            perf_expr = ','.join(line.split(',')[1:]).replace('\n', '')
+            perf_str += "-e " + perf_expr + " "
+
+        command = perf_str + command
+        print(command)
+        # command = "perf stat -e instructions " +\
+        #         "-e r531010 -e r538010 "  +\
+        #         "-e cpu/config=0x5301b7,config1=0x3f80408fff,name=OFFCORE_RESPONSE_0:LLC_MISS_LOCAL/ " +\
+        #         "-e cycles " +\
+        #         "-e fp_arith_inst_retired.128b_packed_double " +\
+        #         "-e fp_arith_inst_retired.256b_packed_double " +\
+        #         "-e fp_arith_inst_retired.scalar_double " +\
+        #         "-e offcore_response.demand_code_rd.l3_miss_local_dram.any_snoop " + command
+        type_from_ml = "find=compact"
+
     # append type in submission
     types.append(type_from_ml)
+    print()
     print(types)
+    print()
