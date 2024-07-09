fef4819736026e7a9c0c87482574cd5f58b01969
nikosT <ntriantafyl@cslab.ece.ntua.gr>
2024-07-10 00:17:37 +0300

    Modify core sort function of compact and spread to perform faster decisions

--- oar/kao/custom_scheduling.py
+++ oar/kao/custom_scheduling.py
@@ -2,26 +2,9 @@ from procset import ProcSet
 
 from oar.lib.globals import get_logger
 from oar.lib.hierarchy import find_resource_hierarchies_scattered
+import itertools
 
 logger = get_logger("oar.custom_scheduling")
-
-
-def path(itvs, hy, reverse=True):
-    """
-    Find the path leading to a resource within the hierarchy.
-    e.g. hy = {'nodes': [ProcSet((0, 7)), ProcSet((8, 15))], 'cpu': [ProcSet((0, 3)), ProcSet((4, 7)), ProcSet((8, 11)), ProcSet((12, 15))], 'core': [ProcSet(0), ProcSet(1), ProcSet(2), ProcSet(3), ProcSet(4), ProcSet(5), ProcSet(6), ProcSet(7), ProcSet(8), ProcSet(9), ProcSet(10), ProcSet(11), ProcSet(12), ProcSet(13), ProcSet(14), ProcSet(15)], 'resource_id': [ProcSet(0), ProcSet(1), ProcSet(2), ProcSet(3), ProcSet(4), ProcSet(5), ProcSet(6), ProcSet(7), ProcSet(8), ProcSet(9), ProcSet(10), ProcSet(11), ProcSet(12), ProcSet(13), ProcSet(14), ProcSet(15)]}
-    e.g. itvs = ProcSet((8,11))
-    path(itvs, hy) = [ProcSet((8,15)), ProcSet((8,11))]
-    """
-    acc = []
-    for lv in hy:
-        for rsc in hy[lv]:
-            if itvs.issubset(rsc):
-                acc.append(rsc)
-                break
-
-    return sorted(acc, key=lambda x: len(x), reverse=reverse)
-
 
 def compact(itvs_slots, hy_res_rqts, hy, beginning_slotset, reverse=True):
     """
@@ -49,17 +32,16 @@ def compact(itvs_slots, hy_res_rqts, hy, beginning_slotset, reverse=True):
 
         itvs_cts_slots = constraints & itvs_slots
 
-        # Select unused resources first (top-down).
-        hy_levels = map(
-            lambda x: sorted(
-                x,
-                key=lambda i: [
-                    len(prev & itvs_cts_slots) for prev in path(i, hy, reverse=reverse)
-                ],
-                reverse=reverse,
-            ),
-            hy_levels,
-        )
+        hy_nodes = sorted(hy['network_address'], key=lambda i: len(i & itvs_cts_slots), reverse=reverse)
+
+        hy_levels = []
+        for node in hy_nodes:
+            n_cpus=list(filter(lambda p: p.issubset(node),hy['cpu']))
+            n_cpus=sorted(n_cpus, key=lambda i: len(i & itvs_cts_slots), reverse=reverse)
+            hy_levels+=list(map(ProcSet, itertools.chain.from_iterable(map(iter, n_cpus))))
+
+        hy_levels=[hy_levels]
+
         res = find_resource_hierarchies_scattered(
             itvs_cts_slots, list(hy_levels), hy_nbs
         )
@@ -105,18 +87,16 @@ def spread(itvs_slots, hy_res_rqts, hy, beginning_slotset, reverse=False):
 
         # Select unused resources first (top-down).
         try:
-            hy_levels = list(
-                map(
-                    lambda x: sorted(
-                        x,
-                        key=lambda i: [
-                            len(prev & itvs_cts_slots2) for prev in path(i, hy)
-                        ],
-                        reverse=not reverse,
-                    ),
-                    hy_levels,
-                )
-            )
+            hy_nodes = sorted(hy['network_address'], key=lambda i: len(i & itvs_cts_slots), reverse=reverse)
+
+            hy_levels = []
+            for node in hy_nodes:
+                n_cpus=list(filter(lambda p: p.issubset(node),hy['cpu']))
+                n_cpus=sorted(n_cpus, key=lambda i: len(i & itvs_cts_slots), reverse=reverse)
+                hy_levels+=list(map(ProcSet, itertools.chain.from_iterable(map(iter, n_cpus))))
+
+            hy_levels=[hy_levels]
+
         except Exception as e:
             logger.info(e)
 
